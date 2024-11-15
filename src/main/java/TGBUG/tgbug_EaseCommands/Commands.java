023c8e30c79b8ad90dcec1b8c3fb642078c76651
TGBUG <3770955706@qq.com>
2024-11-15 23:27:51 +0800

    修复部分bug，优化部分逻辑

--- src/main/java/TGBUG/tgbug_EaseCommands/Commands.java
+++ src/main/java/TGBUG/tgbug_EaseCommands/Commands.java
@@ -5,6 +5,7 @@ import org.bukkit.command.*;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,7 @@ import java.util.Map;
 public class Commands implements CommandExecutor, TabCompleter {
     private ConfigManger configManger;
     private final TGBUG_EaseCommands plugin;
+    private final List<String> cachedCommands = new ArrayList<>();
 
     public Commands(ConfigManger configManger, TGBUG_EaseCommands plugin) {
         this.plugin = plugin;
@@ -26,10 +28,11 @@ public class Commands implements CommandExecutor, TabCompleter {
         } else {
             switch (args[0].toLowerCase()) {
                 case "reload":
-                    unregisterCommands();
-                    configManger = configManger.loadConfig();
-                    registerCommands();
-                    plugin.getLogger().info("已重新加载配置文件");
+                    if (unregisterCommands()) {
+                        configManger = configManger.loadConfig();
+                        registerCommands();
+                        plugin.getLogger().info("已重新加载配置文件");
+                    }
                     return true;
 
                 default:
@@ -56,71 +59,72 @@ public class Commands implements CommandExecutor, TabCompleter {
         for (Map<?, ?> commandMap : commands) {
             for (Map.Entry<?, ?> entry : commandMap.entrySet()) {
                 String commandName = String.valueOf(entry.getKey());
-                Object value = entry.getValue();
 
-                CommandExecutor executor = (sender, command, label, args) -> {
-                    if (value instanceof Map<?, ?> dataMap) {
-                        if (dataMap.containsKey("full_command")) {
-                            String fullCommand = (String) dataMap.get("full_command");
-                            Bukkit.dispatchCommand(sender, fullCommand);
-                            return true;
-                        }
-                        if (dataMap.containsKey("subcommands")) {
-                            List<Map<?, ?>> subCommands = (List<Map<?, ?>>) dataMap.get("subcommands");
+                if (!cachedCommands.contains(commandName)) {
+                    cachedCommands.add(commandName);
+                    Object value = entry.getValue();
 
-                            if (args.length == 0) {
+                    CommandExecutor executor = (sender, command, label, args) -> {
+                        if (value instanceof Map<?, ?> dataMap) {
+                            if (dataMap.containsKey("full_command")) {
+                                String fullCommand = (String) dataMap.get("full_command");
+                                Bukkit.dispatchCommand(sender, fullCommand);
+                                return true;
+                            }
+                            if (dataMap.containsKey("subcommands")) {
+                                List<Map<?, ?>> subCommands = (List<Map<?, ?>>) dataMap.get("subcommands");
+
+                                if (args.length == 0) {
+                                    sender.sendMessage("未知命令");
+                                    return false;
+                                }
+
+                                for (Map<?, ?> subCommandMap : subCommands) {
+                                    if (subCommandMap.containsKey("subcommand") && subCommandMap.containsKey("full_command")) {
+                                        String subCommand = (String) subCommandMap.get("subcommand");
+                                        String fullCommand = (String) subCommandMap.get("full_command");
+
+                                        if (args[0].equalsIgnoreCase(subCommand)) {
+                                            Bukkit.dispatchCommand(sender, fullCommand);
+                                            return true;
+                                        }
+                                    }
+                                }
+
                                 sender.sendMessage("未知命令");
                                 return false;
                             }
+                        }
+                        return false;
+                    };
 
-                            for (Map<?, ?> subCommandMap : subCommands) {
-                                if (subCommandMap.containsKey("subcommand") && subCommandMap.containsKey("full_command")) {
+                    TabCompleter tabCompleter = (sender, command, alias, args) -> {
+                        List<String> completions = new ArrayList<>();
+
+                        if (value instanceof Map<?, ?> dataMap && dataMap.containsKey("subcommands") && args.length == 1) {
+                            List<Map<String, Object>> subCommands = (List<Map<String, Object>>) dataMap.get("subcommands");
+
+                            for (Map<String, Object> subCommandMap : subCommands) {
+                                if (subCommandMap.containsKey("subcommand")) {
                                     String subCommand = (String) subCommandMap.get("subcommand");
-                                    String fullCommand = (String) subCommandMap.get("full_command");
-
-                                    if (args[0].equalsIgnoreCase(subCommand)) {
-                                        Bukkit.dispatchCommand(sender, fullCommand);
-                                        return true;
+                                    if (subCommand != null && subCommand.toLowerCase().startsWith(args[0].toLowerCase())) {
+                                        completions.add(subCommand);
                                     }
                                 }
                             }
-
-                            sender.sendMessage("未知命令");
-                            return false;
                         }
-                    }
-                    return false;
-                };
 
-                TabCompleter tabCompleter = (sender, command, alias, args) -> {
-                    List<String> completions = new ArrayList<>();
+                        return completions;
+                    };
 
-                    if (value instanceof Map<?, ?> dataMap && dataMap.containsKey("subcommands") && args.length == 1) {
-                        List<Map<String, Object>> subCommands = (List<Map<String, Object>>) dataMap.get("subcommands");
-
-                        for (Map<String, Object> subCommandMap : subCommands) {
-                            if (subCommandMap.containsKey("subcommand")) {
-                                String subCommand = (String) subCommandMap.get("subcommand");
-                                if (subCommand != null && subCommand.toLowerCase().startsWith(args[0].toLowerCase())) {
-                                    completions.add(subCommand);
-                                }
-                            }
-                        }
-                    }
-
-                    return completions;
-                };
-
-                CustomCommand command = new CustomCommand(commandName, executor, tabCompleter);
-                Bukkit.getCommandMap().register(commandName, command);
+                    CustomCommand command = new CustomCommand(commandName, executor, tabCompleter);
+                    Bukkit.getCommandMap().register(commandName, command);
+                }
             }
         }
     }
 
-
-    public void unregisterCommands() {
-        List<Map<?, ?>> commands = configManger.getCommands();
-
+    public boolean unregisterCommands() {
         try {
             Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
             commandMapField.setAccessible(true);
@@ -130,21 +134,22 @@ public class Commands implements CommandExecutor, TabCompleter {
             knownCommandsField.setAccessible(true);
             Map<String, Command> knownCommands = (Map<String, Command>) knownCommandsField.get(commandMap);
 
-            for (Map<?, ?> commandMapConfig : commands) {
-                for (Map.Entry<?, ?> entry : commandMapConfig.entrySet()) {
-                    if (entry.getKey() instanceof String commandName) {
-                        Command command = knownCommands.get(commandName);
-                        if (command != null && command.unregister(commandMap)) {
-                            knownCommands.remove(commandName);
-                        } else {
-                            System.out.println("注销 " + commandName + " 失败，请向插件作者反馈");
-                        }
-                    }
+            Iterator<String> iterator = cachedCommands.iterator();
+            while (iterator.hasNext()) {
+                String commandName = iterator.next();
+                Command command = knownCommands.get(commandName);
+                if (command != null && command.unregister(commandMap)) {
+                    iterator.remove();
+                } else {
+                    plugin.getLogger().warning("无法注销命令: " + commandName + " 请向插件作者反馈");
+                    return false;
                 }
             }
         } catch (Exception e) {
+            plugin.getLogger().severe("注销命令时发生错误: " + e.getMessage() + "请向插件作者反馈");
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
-
 }
